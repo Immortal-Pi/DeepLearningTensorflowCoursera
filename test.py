e9a33f469727def5b21093f1add8b8244c3b891d
unknown <26.amruth@gmail.com>
2024-07-02 21:52:33 +0530

    LSTM

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
-
+# import tensorflow_datasets as tfds
 print(tf.config.list_physical_devices('GPU'))
+# print(tfds.__version__)
 
 print(tf.__version__)
 
