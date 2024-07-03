891a4338294cc07930debac8a0bb9f5fa596fee7
unknown <26.amruth@gmail.com>
2024-07-03 17:03:20 +0530

    CNN RNN

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-# import tensorflow_datasets as tfds
-print(tf.config.list_physical_devices('GPU'))
-# print(tfds.__version__)
+# import tensorflow as tf
+# import matplotlib.pyplot as plt
+# import numpy as np
+# # import tensorflow_datasets as tfds
+# print(tf.config.list_physical_devices('GPU'))
+# # print(tfds.__version__)
+#
+# print(tf.__version__)
+#
+#
+import tensorflow_datasets as tfds
 
-print(tf.__version__)
+builder = tfds.builder('imdb_reviews/subwords8k')
+builder.download_and_prepare(download_dir='/path/to/downloads', download_config=tfds.download.DownloadConfig(delete_downloaded=True))
 
